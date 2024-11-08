b934d4a810aaa6437774660774e5ce39b3c662c1
Elliot Adinortey Agbettor <77128584+E-Agbettor@users.noreply.github.com>
2024-11-08 10:58:04 +0000

    Update image.js

--- image.js
+++ image.js
@@ -1,1 +1,67 @@
-
+export const images = [
+    {
+        full: './img/abstract_autumn.jpg',
+        thumb: './img/abstract_autumn.jpg',
+        caption: 'Abstract autumn beauty multi colored leaf vein pattern'
+    },
+    {
+        full: './img/fieldGarden.jpg',
+        thumb: './img/fieldGarden.jpg',
+        caption: 'A garden field'
+    },
+    {
+        full: './img/couple.jpg',
+        thumb: './img/couple.jpg',
+        caption: 'Couple sits by lake gazing full moon surrounded by trees'
+    },
+    {
+        full: './img/mysterious_dark_forest.jpg',
+        thumb: './img/mysterious_dark_forest.jpg',
+        caption: 'Mysterious dark forest with stars in the sky night'
+    },
+    {
+        full: './img/oak_tree.jpg',
+        thumb: './img/oak_tree.jpg',
+        caption: 'One big oak tree'
+    },
+    {
+        full: './img/Chameleon.jpg',
+        thumb: './img/Chameleon.jpg',
+        caption: 'Attractive chamel'
+    },
+    {
+        full: './img/analog.jpg',
+        thumb: './img/analog.jpg',
+        caption: 'Capturing memory'
+    },
+    {
+        full: './img/coupleBoatFullMoon.jpg',
+        thumb: './img/coupleBoatFullMoon.jpg',
+        caption: 'couple boat full moon'
+    },
+    {
+        full: './img/fieldGarden.jpg',
+        thumb: './img/fieldGarden.jpg',
+        caption: 'A Field Garden'
+    },
+    {
+        full: './img/freeImages.jpg',
+        thumb: './img/freeImages.jpg',
+        caption: 'The randoms serves the good times'
+    },
+    {
+        full: './img/MoonLake.jpg',
+        thumb: './img/MoonLake.jpg',
+        caption: 'A Moon Lake'
+    },
+    {
+        full: './img/pexels-_souvenir.jpg',
+        thumb: './img/pexels-_souvenir.jpg',
+        caption: 'A good sunset at the bank of the river'
+    },
+    {
+        full: './img/tree.jpg',
+        thumb: './img/tree.jpg',
+        caption: 'Settling sunset and a tree'
+    },
+];
